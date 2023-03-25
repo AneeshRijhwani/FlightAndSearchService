7e460055b781724877229736da2ef22bd6e2431b
AneeshRijhwani25 <7851950798aneesh@gmail.com>
2023-03-25 11:10:09 +0530

    Added City controller

---
+++ src/controllers/city-controllers.js
@@ -0,0 +1,95 @@
+const { response } = require("express");
+const {CityService} = require("../services/index");
+const cityService=new CityService();
+
+const create = async(req,res)=>{
+    try {
+        const city= await cityService.crateCity(req.body);
+        return res.status(201).json({
+            data:city,
+            success:true,
+            message:"Successfully created a city",
+            err:{}
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success: false,
+            message:"Not able to create a City",
+            err:error
+        })
+    }
+    
+}
+// delete -> /city/:id in  request url
+const destroy =async (req,res)=>{
+    try {
+        const response= await cityService.deleteCity(req.params.id);
+        return res.status(200).json({
+            data:e=response,
+            success:true,
+            message:"Successfully destroyed the city",
+            err:{}
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success: false,
+            message:"Not able to destroy the City",
+            err:error
+        })
+    }
+
+}
+const get =async (req,res)=>{
+    try {
+        const city= await cityService.getCity(req.params.id);
+        return res.status(201).json({
+            data:city,
+            success:true,
+            message:"Successfully fetched the city",
+            err:{}
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success: false,
+            message:"Not able to fetch the City",
+            err:error
+        })
+    }
+
+}
+const update =async (req,res)=>{
+    try {
+        const city= await cityService.updateCity(req.params.id,req.body);
+        return res.status(201).json({
+            data:city,
+            success:true,
+            message:"Successfully updated the city",
+            err:{}
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success: false,
+            message:"Not able to update the City",
+            err:error
+        })
+    }
+}
+
+module.exports={
+    create,
+    destroy,
+    get,
+    update
+}
